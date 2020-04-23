a749e6a46d3fdac6daedc4b683abdbb10aeec6c7
Charles Lechasseur <charles.lechasseur@ingeno.ca>
2020-04-23 14:14:43 -0400

    Day 1

--- src/test/kotlin/AdventOfCode2019.kt
+++ src/test/kotlin/AdventOfCode2019.kt
@@ -3,6 +3,11 @@ import kotlin.test.Test
 class AdventOfCode2019 {
     @Test
     fun `day 1, part 1`() {
-        Day1.day1_1()
+        Day1.part1()
+    }
+
+    @Test
+    fun `day 1, part 2`() {
+        Day1.part2()
     }
 }
